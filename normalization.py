f022f849eed3d04134217c196dd29f6781f007d3
TheEmilia <emiliaalexandrakiller@gmail.com>
2023-03-13 11:47:55 +0200

    added normalization code (untested)

--- normalization.py
+++ normalization.py
@@ -1,10 +1,34 @@
+import numpy as np
+
+
 def feature_scale(features):
-    pass
+    # x_i/max
+    scaled_features = []
+    for feature in features:
+        scaled_features.append((feature / max(features)))
+
+    return scaled_features
 
 
 def mean_normalization(features):
-    pass
+    # (x_i - mean_i) / (max-min)
+    scaled_features = []
+    feature_mean = np.mean(features)
+    feature_range = max(features) - min(features)
+
+    for feature in features:
+        scaled_features.append(((feature - feature_mean) / feature_range))
+
+    return scaled_features
 
 
 def zscore_normalization(features):
-    pass
+    # (x_i - mean_i) / std. deviation
+    scaled_features = []
+    feature_mean = np.mean(features)
+    std_dev = np.std(features)
+
+    for feature in features:
+        scaled_features.append(((feature - feature_mean) / (std_dev)))
+
+    return scaled_features
